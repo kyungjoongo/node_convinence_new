1c146dcb7b93e670c0d1eeccdf6257a8e34cfb2d
gilzako <iamjessica77777@gmail.com>
2019-11-18 05:50:44 +0900

    flkgdflkgfd

--- file_delete_right_now.js
+++ file_delete_right_now.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const directory = './images';
+let directory = './images';
 
 fs.readdir(directory, (err, files) => {
     if (err) throw err;
@@ -12,5 +12,18 @@ fs.readdir(directory, (err, files) => {
         });
     }
 });
+
+directory = './public/images';
+
+fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+            if (err) throw err;
+        });
+    }
+});
+
 
 console.log('osdkfdokf=====>delete')
--- file_delete_schedulaer.js
+++ file_delete_schedulaer.js
@@ -4,7 +4,19 @@ var j = schedule.scheduleJob('45 * * * *', function(){
     const fs = require('fs');
     const path = require('path');
 
-    const directory = './images';
+    let directory = './images';
+
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+
+    directory = './public/images';
 
     fs.readdir(directory, (err, files) => {
         if (err) throw err;
