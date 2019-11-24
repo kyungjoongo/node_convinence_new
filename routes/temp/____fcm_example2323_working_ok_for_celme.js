f43550b808dea90ccd67111e2c6275148075b711
gilzako <iamjessica77777@gmail.com>
2019-11-24 17:44:31 +0900

    flkgdflkgfd

--- routes/temp/____fcm_example2323_working_ok_for_celme.js
+++ routes/temp/____fcm_example2323_working_ok_for_celme.js
@@ -1,12 +1,13 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./face-celub-firebase-adminsdk-hqtu6-3401d7b3a9.json");
-
+var serviceAccount = require("./face-celub-firebase-adminsdk-spcv7-6195fe916f.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://face-celub.firebaseio.com"
+    //databaseURL: "<your database URL here>"
 });
+
+
 
 
 let message = {
@@ -19,11 +20,13 @@ let message = {
 };
 
 // Send a message to devices subscribed to the provided topic.
-admin.messaging().send(message).then((response) => {
+admin.messaging().send(message)
+    .then((response) => {
 
 
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response + "---->");
-}).catch((error) => {
-    console.log('Error sending message:', error);
-});
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response + "---->");
+    })
+    .catch((error) => {
+        console.log('Error sending message:', error);
+    });
